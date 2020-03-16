16429bb517f1610c5716e2752f568da0a30db6de
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-03-16 14:06:21 +0100

    Initialize project

---
+++ src/main/java/com/aston/AstonSpringMYoussfiApplication.java
@@ -0,0 +1,13 @@
+package com.aston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonSpringMYoussfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonSpringMYoussfiApplication.class, args);
+	}
+
+}
